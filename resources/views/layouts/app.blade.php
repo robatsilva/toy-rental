29df6620dc06733e328294c9e45b2352c4147f66
robinson <robatsilva@hotmail.com>
2018-12-10 19:50:41 -0200

    removendo registro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -107,7 +107,7 @@
                     <!-- Authentication Links -->
                     @if (Auth::guest())
                         <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Registro</a></li>
+                        <!-- <li><a href="{{ url('/register') }}">Registro</a></li> -->
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
