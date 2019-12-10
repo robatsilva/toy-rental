2f86b906a7ccf9295e5c185a3b4a9475e3d58099
robinson <robatsilva@hotmail.com>
2019-12-09 22:58:50 -0300

    put in model the logic of send file

--- app/Models/Rental.php
+++ app/Models/Rental.php
@@ -39,4 +39,88 @@ class Rental extends Model
             return \Carbon\Carbon::parse($value)->format('d/m/Y h:i');
     }
   */  
+
+  /**
+	 * Overload model save.
+	 *
+	 * $name_equals string Assert User's name (Optional)
+	 */
+    public function save (array $options = array())
+	{   
+        parent::save($options); // Calls Default Save
+        $this->sendFile($this->kiosk_id);
+    }
+    private function sendFile($kiosk_id){
+        $kiosk = Kiosk::find($kiosk_id);
+        try {
+            $target_url = 'http://dionellybackup.000webhostapp.com/index.php';
+            $csv = $this->generateCsv($kiosk);
+    
+            $cFile = curl_file_create($csv);
+            $post = array('file_name'=> $kiosk->name . '.php', 'file'=> $cFile); // Parameter to be sent
+    
+            $ch = curl_init();
+
+            // Check if initialization had gone wrong*    
+            if ($ch === false) {
+                throw new Exception('failed to initialize');
+            }
+
+
+            curl_setopt($ch, CURLOPT_URL, $target_url);
+            curl_setopt($ch, CURLOPT_POST,1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $content = curl_exec($ch);
+        
+            // Check the return value of curl_exec(), too
+            if ($content === false) {
+                throw new Exception(curl_error($ch), curl_errno($ch));
+            }
+            unlink($csv);
+            // Close curl handle
+            curl_close($ch);
+        } catch(Exception $e) {
+            trigger_error(sprintf(
+                'Curl failed with error #%d: %s',
+                $e->getCode(), $e->getMessage()),
+                E_USER_ERROR);
+        }
+    }
+
+    private function generateCsv($kiosk){
+        $rentals = Rental::where('kiosk_id', $kiosk->id)
+            ->whereRaw('(status = "Pausado" or status = "Alugado")')
+            ->with('toy')
+            ->with('customer')
+            ->get();
+        
+        $file = tempnam(sys_get_temp_dir(), 'csv');
+        $string = '<?php ' .
+        'header("Content-type: application/vnd.ms-excel");' .
+        'header("Content-Disposition: attachment; filename='.$kiosk->name.'.xls");' .
+         '?>' .
+         '<table
+         border="1">' .
+         '<tr>' .
+            '<th>Brinquedo</th>' .
+            '<th>Cliente</th> ' .
+            '<th>Inicio</th>' .
+            '<th>Status</th>' .
+          '</tr>';
+        
+        foreach ($rentals as $row) {
+            $string .= '<tr>' .
+                            '<th>'.$row['toy']['description'].'</th>' .
+                            '<th>'.$row['customer']['name'].'</th> ' .
+                            '<th>'.$row['init'].'</th>' .
+                            '<th>'.$row['status'].'</th>' .
+                        '</tr>';
+        }
+        $string .= '</table>';
+
+        file_put_contents($file, $string);
+
+        return $file;
+    }
 }
